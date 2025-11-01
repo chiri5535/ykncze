28910337256b0fd45c1e75fb964231a6f30b74f6
chiri5535 <chiri5535@gmail.com>
2025-11-01 15:47:17 +0900

    Update lucky colors for zodiac signs

--- src/utils/zodiac.js
+++ src/utils/zodiac.js
@@ -4,84 +4,84 @@ export const getZodiacSign = (month, day) => {
       name: '山羊座', 
       start: [12, 22], 
       end: [1, 19], 
-      lucky: 'ダークパープル',
+      lucky: 'カフェモカブラウン',
       image: 'http://zeny.deci.jp/page/images/seiza/01yagi.png'
     },
     { 
       name: '水瓶座', 
       start: [1, 20], 
       end: [2, 18], 
-      lucky: 'ミントグリーン',
+      lucky: 'アイボリーホワイト',
       image: 'http://zeny.deci.jp/page/images/seiza/02mizugame.png'
     },
     { 
       name: '魚座', 
       start: [2, 19], 
       end: [3, 20], 
-      lucky: 'ローズクォーツピンク',
+      lucky: 'ミスティグレー',
       image: 'http://zeny.deci.jp/page/images/seiza/03uo.png'
     },
     { 
       name: '牡羊座', 
       start: [3, 21], 
       end: [4, 19], 
-      lucky: 'カーネーションピンク',
+      lucky: 'ディープオレンジ',
       image: 'http://zeny.deci.jp/page/images/seiza/04ohitsuji.png'
     },
     { 
       name: '牡牛座', 
       start: [4, 20], 
       end: [5, 20], 
-      lucky: 'マスタードイエロー',
+      lucky: 'ローズブラウン',
       image: 'http://zeny.deci.jp/page/images/seiza/05oushi.png'
     },
     { 
       name: '双子座', 
       start: [5, 21], 
       end: [6, 20], 
-      lucky: 'コバルトブルー',
+      lucky: 'ターコイズグリーン',
       image: 'http://zeny.deci.jp/page/images/seiza/06futago.png'
     },
     { 
       name: '蟹座', 
       start: [6, 21], 
       end: [7, 22], 
-      lucky: 'パールホワイト',
+      lucky: 'ペールピンク',
       image: 'http://zeny.deci.jp/page/images/seiza/07kani.png'
     },
     { 
       name: '獅子座', 
       start: [7, 23], 
       end: [8, 22], 
-      lucky: 'バーガンディ',
+      lucky: 'ネイビーブルー',
       image: 'http://zeny.deci.jp/page/images/seiza/08shishi.png'
     },
     { 
       name: '乙女座', 
       start: [8, 23], 
       end: [9, 22], 
-      lucky: 'オリーブグリーン',
+      lucky: 'マリンブルー',
       image: 'http://zeny.deci.jp/page/images/seiza/09otome.png'
     },
     { 
       name: '天秤座', 
       start: [9, 23], 
       end: [10, 22], 
-      lucky: 'アクアブルー',
+      lucky: 'モーブパープル',
       image: 'http://zeny.deci.jp/page/images/seiza/10tenbin.png'
     },
     { 
       name: '蠍座', 
       start: [10, 23], 
       end: [11, 21], 
-      lucky: 'ワインレッド',
+      lucky: 'ガーネットレッド',
       image: 'http://zeny.deci.jp/page/images/seiza/11sasori.png'
     },
     { 
       name: '射手座', 
       start: [11, 22], 
       end: [12, 21], 
-      lucky: 'ターコイズブルー',
+      lucky: 'ライトグリーン',
       image: 'http://zeny.deci.jp/page/images/seiza/12ite.png'
     }
   ];
