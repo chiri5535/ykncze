c23de8f113fd3293dccfcaf7262c8dbacaff15b3
chiri5535 <chiri5535@gmail.com>
2026-03-31 19:33:30 +0900

    Update lucky colors for zodiac signs

--- src/utils/zodiac.js
+++ src/utils/zodiac.js
@@ -4,84 +4,84 @@ export const getZodiacSign = (month, day) => {
       name: '山羊座', 
       start: [12, 22], 
       end: [1, 19], 
-      lucky: '成果を形にするチャコールグレー',
+      lucky: '信念を穏やかに支えるスモーキーネイビー',
       image: 'http://zeny.deci.jp/page/images/seiza/01yagi.png'
     },
     { 
       name: '水瓶座', 
       start: [1, 20], 
       end: [2, 18], 
-      lucky: '未来への視界を開くクリアスカイブルー',
+      lucky: 'ひらめきを現実につなぐアイスブルー',
       image: 'http://zeny.deci.jp/page/images/seiza/02mizugame.png'
     },
     { 
       name: '魚座', 
       start: [2, 19], 
       end: [3, 20], 
-      lucky: '浄化と直感を強めるシーグリーン',
+      lucky: '心をやさしく浄化するミスティアクア',
       image: 'http://zeny.deci.jp/page/images/seiza/03uo.png'
     },
     { 
       name: '牡羊座', 
       start: [3, 21], 
       end: [4, 19], 
-      lucky: 'スタート運を加速するスカーレット',
+      lucky: '挑戦運を燃やすブライトレッド',
       image: 'http://zeny.deci.jp/page/images/seiza/04ohitsuji.png'
     },
     { 
       name: '牡牛座', 
       start: [4, 20], 
       end: [5, 20], 
-      lucky: '心身の巡りを整えるフレッシュグリーン',
+      lucky: '豊かさの芽を育てるリーフグリーン',
       image: 'http://zeny.deci.jp/page/images/seiza/05oushi.png'
     },
     { 
       name: '双子座', 
       start: [5, 21], 
       end: [6, 20], 
-      lucky: '言葉と縁をつなぐミントブルー',
+      lucky: '会話と好奇心を軽やかにするレモンイエロー',
       image: 'http://zeny.deci.jp/page/images/seiza/06futago.png'
     },
     { 
       name: '蟹座', 
       start: [6, 21], 
       end: [7, 22], 
-      lucky: '心をほどくピーチホワイト',
+      lucky: '安心感とぬくもりをもたらすシェルピンク',
       image: 'http://zeny.deci.jp/page/images/seiza/07kani.png'
     },
     { 
       name: '獅子座', 
       start: [7, 23], 
       end: [8, 22], 
-      lucky: '注目運を呼び込むサンフラワーイエロー',
+      lucky: '魅力を自然に輝かせるサニーオレンジ',
       image: 'http://zeny.deci.jp/page/images/seiza/08shishi.png'
     },
     { 
       name: '乙女座', 
       start: [8, 23], 
       end: [9, 22], 
-      lucky: '整える力を高めるライトグレージュ',
+      lucky: '生活を整え運を磨くペールベージュ',
       image: 'http://zeny.deci.jp/page/images/seiza/09otome.png'
     },
     { 
       name: '天秤座', 
       start: [9, 23], 
       end: [10, 22], 
-      lucky: '魅力と調和を引き出すペールコーラル',
+      lucky: '愛され運と調和を高めるローズピンク',
       image: 'http://zeny.deci.jp/page/images/seiza/10tenbin.png'
     },
     { 
       name: '蠍座', 
       start: [10, 23], 
       end: [11, 21], 
-      lucky: '決断力と再生を促すディーププラム',
+      lucky: '本音と直感を研ぎ澄ますダークワイン',
       image: 'http://zeny.deci.jp/page/images/seiza/11sasori.png'
     },
     { 
       name: '射手座', 
       start: [11, 22], 
       end: [12, 21], 
-      lucky: '追い風と行動力を呼ぶターコイズ',
+      lucky: '未来への扉を開くスプリングターコイズ',
       image: 'http://zeny.deci.jp/page/images/seiza/12ite.png'
     }
   ];
